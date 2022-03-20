0fc7d7ca3d62c14a4febf39a6af820df9cbd27da
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-03-20 13:05:35 +0000

    banner styles and classNames

--- components/banner.js
+++ components/banner.js
@@ -1,25 +1,42 @@
-import style from './banner.module.css';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import styles from './banner.module.css';
 
 const Banner = (props) => {
-  // destructure properties from props
-  const { title, subTitle, imgUrl } = props;
+  const { title, subTitle, imgUrl, videoId } = props;
+  const router = useRouter();
 
   const handleOnPlay = () => {
-    console.log('handleOnPlay');
+    router.push(`video/${videoId}`);
   };
-
   return (
-    <div>
-      <h3>{title}</h3>
-      <h4>{subTitle}</h4>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              {/* <Image
+                src="/static/play_arrow.svg"
+                alt="Play icon"
+                width="32px"
+                height="32px"
+              /> */}
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
-          backgroundImage: `url(${imgUrl})`,
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          backgroundPosition: '50% 50%',
+          backgroundImage: `url(${imgUrl}`,
         }}
       ></div>
     </div>
