27840a788be0d6ac3f54857291c285f00549eff3
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-03-20 13:09:55 +0000

    added google font play icon

--- components/banner.js
+++ components/banner.js
@@ -22,12 +22,12 @@ const Banner = (props) => {
 
           <div className={styles.playBtnWrapper}>
             <button className={styles.btnWithIcon} onClick={handleOnPlay}>
-              {/* <Image
-                src="/static/play_arrow.svg"
-                alt="Play icon"
-                width="32px"
-                height="32px"
-              /> */}
+              <Image
+                src='/static/play_arrow.svg'
+                alt='Play icon'
+                width='32px'
+                height='32px'
+              />
               <span className={styles.playText}>Play</span>
             </button>
           </div>
